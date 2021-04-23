272441d92ae73e3b1db95f19c683f066bd7a1af6
Pio154 <80965673+Pio154@users.noreply.github.com>
2021-04-23 12:01:35 +0200

    Update 11.js

--- 11.js
+++ 11.js
@@ -2,7 +2,7 @@ function prz()
 {
     let Vp = document.getElementById("vp").value;
     let Vk = document.getElementById("vk").value;
-    let zV = Vk - Vp;
     let a = docuemnt.getElementById("awyn");
     a.innerHTML = zV / t;
+    let zV = Vk - Vp;
 }
